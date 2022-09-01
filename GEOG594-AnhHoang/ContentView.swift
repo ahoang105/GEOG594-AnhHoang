2e13fdfd2a172fa977b454cbc71616d875cecc54
ahoang105 <athoang@sdsu.edu>
2022-09-01 11:19:51 -0700

    Initial Commit

---
+++ GEOG594-AnhHoang/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GEOG594-AnhHoang
+//
+//  Created by tram anh on 9/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GEOG594-AnhHoang/GEOG594_AnhHoangApp.swift
@@ -0,0 +1,17 @@
+//
+//  GEOG594_AnhHoangApp.swift
+//  GEOG594-AnhHoang
+//
+//  Created by tram anh on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct GEOG594_AnhHoangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
